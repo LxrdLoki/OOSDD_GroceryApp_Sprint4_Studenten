fb4033c0d0a9575b8e954e735fef65acb2d63361
LxrdLoki <melvinVD69@outlook.com>
2025-10-02 10:50:40 +0200

    Added usecase 11 functionality, looping through all products and products within a grocerylist to check how many times it is within a grocerylist

--- Grocery.App/ViewModels/BestSellingProductsViewModel.cs
+++ Grocery.App/ViewModels/BestSellingProductsViewModel.cs
@@ -2,16 +2,19 @@
 using Grocery.Core.Interfaces.Services;
 using Grocery.Core.Models;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 
 namespace Grocery.App.ViewModels
 {
     public partial class BestSellingProductsViewModel : BaseViewModel
     {
         private readonly IGroceryListItemsService _groceryListItemsService;
+        private readonly IBestSellingProductsService _bestSellingProductsService;
         public ObservableCollection<BestSellingProducts> Products { get; set; } = [];
-        public BestSellingProductsViewModel(IGroceryListItemsService groceryListItemsService)
+        public BestSellingProductsViewModel(IGroceryListItemsService groceryListItemsService, IBestSellingProductsService bestSellingProductsService)
         {
             _groceryListItemsService = groceryListItemsService;
+            _bestSellingProductsService = bestSellingProductsService;
             Products = [];
             Load();
         }
@@ -19,7 +22,7 @@ namespace Grocery.App.ViewModels
         public override void Load()
         {
             Products.Clear();
-            foreach (BestSellingProducts item in _groceryListItemsService.GetBestSellingProducts())
+            foreach (BestSellingProducts item in _bestSellingProductsService.GetBestSellingProducts())
             {
                 Products.Add(item);
             }
