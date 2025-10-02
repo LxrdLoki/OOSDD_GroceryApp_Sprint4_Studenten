3bfa884ea6088fb9624f1c242209178c0b74a283
LxrdLoki <melvinVD69@outlook.com>
2025-10-02 22:04:18 +0200

    Fixed usecase10 by using ImageButton instead of Images, to trigger a click event. Made stock and amount properties Observable.

--- Grocery.Core/Models/Product.cs
+++ Grocery.Core/Models/Product.cs
@@ -2,7 +2,14 @@
 {
     public class Product : Model
     {
-        public int Stock { get; set; }
+        // Replace ObservableProperty<int> with a standard property and use SetProperty for change notification
+        private int _stock;
+        public int Stock
+        {
+            get => _stock;
+            set => SetProperty(ref _stock, value);
+        }
+
         public Product(int id, string name, int stock) : base(id, name)
         {
             Stock = stock;
